3c95fc29473931d2979d51efb2707de3a7122f5f
Vikas Sharma <Vikaskumarasharma06470@gmail.com>
2021-08-07 23:49:33 +0530

    8th commit

--- src/Components/Contactfrom.js
+++ src/Components/Contactfrom.js
@@ -92,7 +92,7 @@ const Contactfrom = () => {
 
   return (
     <>
-    <h1 className="text-center ">Contacts From</h1>
+    <h1 className="text-center mt-3">Contacts From</h1>
       <div className="container w-50 mt-5">
         <form>
           <div className="form-group">
